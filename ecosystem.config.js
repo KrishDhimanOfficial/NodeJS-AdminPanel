2213b9b0b25e867de683c202bd9e89c4da7de801
KrishDhimanOfficial <krrishdhiman841@gmail.com>
2025-10-09 17:31:14 +0530

    add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+export default {
+    apps: [
+        {
+            name: "ADMINPANEL",
+            script: './bin/www',
+            instances: "max",            // Or 1 for single instance
+            autorestart: true,
+            watch: false,
+            max_memory_restart: "500M",
+            exec_mode: "fork",           // Clustering with ES modules can be tricky
+            node_args: "--experimental-specifier-resolution=node",
+            env: {
+                NODE_ENV: "production",
+                PORT: process.env.PORT || 3000
+            }
+        }
+    ]
+}
